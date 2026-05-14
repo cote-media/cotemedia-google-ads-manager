8653fad0ee259e170c8fbfd0e93c8486b32c701f
Russell Cote <russ@cotemedia.com>
2026-05-14 13:51:44 -0400

    Fix Meta OAuth scopes

--- src/app/api/meta/auth/route.ts
+++ src/app/api/meta/auth/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
   const clientId = searchParams.get('clientId')
   const appId = process.env.META_APP_ID!
   const redirectUri = process.env.NEXTAUTH_URL + '/api/meta/callback'
-  const scope = 'ads_read,ads_management,business_management,read_insights'
+  const scope = 'ads_read,ads_management,business_management'
   const state = Buffer.from(JSON.stringify({ clientId, email: session.user.email })).toString('base64')
   const authUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}&state=${state}&response_type=code`
   return NextResponse.redirect(authUrl)
